dfeffc2bf3baa7150f2cbe54a6f3b26911398ad2
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-11 22:10:55 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,24 +1,25 @@
 (function(){
     window.lampa_provider = {
+        type: 'video',
         name: 'UA Online',
         version: '1.0.0',
-        type: 'video',
         logo: 'https://cdn-icons-png.flaticon.com/512/1179/1179069.png',
         description: 'Пошук і перегляд з uakino.me та uaserials.pro',
 
-        get: async function(imdb_id, type, season, episode, onGet){
-            onGet([{ 
-                title: 'Джерело з UAKINO', 
-                url: 'https://uakino.me', 
-                file: 'https://some-video-host.com/sample.mp4', 
-                quality: 'HD',
-                info: 'UAKINO (тест)', 
-                player: true 
-            }])
+        get: async function(imdb_id, type, season, episode, callback){
+            callback([
+                {
+                    title: 'Тестова серія',
+                    file: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+                    quality: 'HD',
+                    info: 'Тест (uakino)',
+                    player: true
+                }
+            ])
         },
 
-        play: function(item, onPlay){
-            onPlay(item.file)
+        play: function(item, callback){
+            callback(item.file)
         }
     }
 })();
