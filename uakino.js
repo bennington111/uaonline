6fe45bd416706598f44aaa66eb0309198bb44beb
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-11 20:52:04 +0300

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -1,15 +1,33 @@
-search: async function(query, onSearchResult){
-    let results = [
-        {
-            title: `Тестовий результат для "${query}" (uakino)`,
-            url: 'https://uakino.me/test-video',
-            poster: '',
-            description: 'Це фейковий результат для перевірки',
-            quality: 'HD',
-            type: 'video'
-        }
-    ]
+(function(){
+    let uaonline = {
+        type: 'video',
+        name: 'UA Online',
+        version: '1.0.0',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1179/1179069.png',
+        description: 'Онлайн-пошук з uakino.me та uaserials.pro',
 
-    console.log('[UA Online] Тестовий результат:', results)
-    onSearchResult(results)
-}
+        search: async function(query, onSearchResult){
+            let results = []
+            results.push({
+                title: 'Шрек (uakino)',
+                url: 'https://uakino.me/123-shrek.html',
+                poster: '',
+                description: 'Тестовий результат',
+                quality: '',
+                type: 'video'
+            })
+            onSearchResult(results)
+        },
+
+        item: async function(item, onItemReady){
+            onItemReady([{
+                file: 'https://some-stream-url.com/video.mp4',
+                title: 'Дивитися',
+                type: 'video'
+            }])
+        }
+    }
+
+    if (window.plugin) window.plugin(uaonline)
+    else window.addEventListener('plugin', () => window.plugin(uaonline))
+})();
