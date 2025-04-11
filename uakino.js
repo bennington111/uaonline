a4823993018b2439815853f3eb18ab911006e61f
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-11 21:09:49 +0300

    Update uakino.js

--- uakino.js
+++ uakino.js
@@ -4,27 +4,27 @@
         name: 'UA Online',
         version: '1.0.0',
         icon: 'https://cdn-icons-png.flaticon.com/512/1179/1179069.png',
-        description: 'Онлайн-пошук з uakino.me та uaserials.pro',
+        description: 'Тестовий UA плагін',
 
         search: async function(query, onSearchResult){
-            console.log('[UA Online] Запит:', query)
-            let results = []
-            results.push({
-                title: query + ' (uakino)',
-                url: 'https://example.com/test',
-                poster: '',
-                description: 'Тестовий результат',
-                quality: '',
-                type: 'video'
-            })
-            onSearchResult(results)
+            console.log('[UA Online] Test search:', query)
+            onSearchResult([
+                {
+                    title: query + ' (UA Test)',
+                    url: 'https://example.com/test',
+                    type: 'video',
+                    poster: '',
+                    description: 'Тестовий результат',
+                    quality: ''
+                }
+            ])
         },
 
         item: async function(item, onItemReady){
-            console.log('[UA Online] item:', item)
+            console.log('[UA Online] Test item:', item)
             onItemReady([{
-                file: 'https://test-stream.com/video.mp4',
-                title: 'Перегляд',
+                title: 'UA Stream',
+                file: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
                 type: 'video'
             }])
         }
