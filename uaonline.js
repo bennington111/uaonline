ddae1c165c038166cf2761f98fea08c9cd02ec54
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-12 21:19:12 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,62 +1,29 @@
 (function(){
-    const uaonline = {
-        version: '1.0.0',
+    window.lampa_provider = {
         type: 'video',
         name: 'UA Online',
-        icon: 'https://cdn-icons-png.flaticon.com/512/1179/1179069.png',
-        description: 'Онлайн-джерела з uakino.me та uaserials.pro',
+        version: '1.0.0',
+        logo: 'https://cdn-icons-png.flaticon.com/512/1179/1179069.png',
+        description: 'Пошук і перегляд з uakino.me та uaserials.pro',
 
-        component: function(component){
-            let query = component.item?.title || component.item?.name || '';
-            let results = [];
-
-            function append(file, label){
-                results.push({
-                    title: label || 'UA Online',
-                    file: file,
-                    url: file,
+        get: async function(imdb_id, type, season, episode, callback){
+            // Тут реалізуйте логіку пошуку джерел за допомогою imdb_id або назви фільму
+            // Наприклад, використовуючи fetch для отримання даних з uakino.me та uaserials.pro
+            // Після отримання даних, викликайте callback з масивом джерел
+            callback([
+                {
+                    title: 'Тестова серія',
+                    file: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
                     quality: 'HD',
-                    stream_type: 'hls',
-                    type: 'video'
-                });
-            }
+                    info: 'Тест (uakino)',
+                    player: true
+                }
+            ]);
+        },
 
-            // uakino.me
-            fetch(`https://corsproxy.io/?https://uakino.me/index.php?do=search&subaction=search&story=${encodeURIComponent(query)}`)
-                .then(r => r.text()).then(html => {
-                    let doc = new DOMParser().parseFromString(html, 'text/html');
-                    let link = doc.querySelector('.short .title a');
-                    if(link){
-                        fetch(`https://corsproxy.io/?${link.href}`).then(r => r.text()).then(page => {
-                            let iframe = page.match(/<iframe.+?src="([^"]+)"/);
-                            if(iframe && iframe[1]) append(iframe[1], 'uakino');
-                            finish();
-                        });
-                    } else finish();
-                }).catch(finish);
-
-            // uaserials.pro
-            fetch(`https://corsproxy.io/?https://uaserials.pro/search?query=${encodeURIComponent(query)}`)
-                .then(r => r.text()).then(html => {
-                    let doc = new DOMParser().parseFromString(html, 'text/html');
-                    let link = doc.querySelector('.ser-thumbnail a');
-                    if(link){
-                        fetch(`https://corsproxy.io/?${link.href}`).then(r => r.text()).then(page => {
-                            let iframe = page.match(/<iframe.+?src="([^"]+)"/);
-                            if(iframe && iframe[1]) append(iframe[1], 'uaserials');
-                            finish();
-                        });
-                    } else finish();
-                }).catch(finish);
-
-            // викликаємо результат
-            function finish(){
-                if(results.length) component.onComponentReady(results);
-                else component.onComponentReady([]);
-            }
+        play: function(item, callback){
+            // Тут реалізуйте логіку відтворення вибраного джерела
+            callback(item.file);
         }
     };
-
-    if(window.plugin) window.plugin(uaonline);
-    else window.addEventListener('plugin', () => window.plugin(uaonline));
 })();
