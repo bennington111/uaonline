456b2a017feedd58cfb43ad282d21f5ef81250da
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-13 21:10:09 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,23 +1,39 @@
-(function () {
-    function uaonline_component() {
-        return {
+(function(){
+    function startPlugin(){
+        var manifest = {
             type: 'video',
             version: '1.0.0',
-            name: 'UA Online',
-            description: 'Перегляд з uakino та uaserials',
-            component: 'uaonline',
-            onItem: function (object, callback) {
-                callback([
-                    {
-                        title: 'Тестовий файл UA Online',
-                        file: 'https://example.com/test.mp4',
-                        type: 'video'
-                    }
-                ]);
+            name: 'Онлайн UA Online',
+            description: 'Джерело з сайтів uakino та uaserials',
+            component: 'uaonline'
+        }
+
+        Lampa.Plugin(manifest, function(plugin){
+            plugin.component = function(name){
+                if(name === 'uaonline') return component
             }
-        };
+
+            return plugin
+        })
     }
 
-    if (window.plugin) window.plugin(uaonline_component());
-    else window.addEventListener('plugin', () => window.plugin(uaonline_component()));
+    var component = {
+        item: function(object, resolve, reject) {
+            // Тестове джерело для прикладу, заміни потім на реальні URL
+            resolve([
+                {
+                    title: 'UAkino (1080p)',
+                    url: 'https://example.com/stream.mp4',
+                    quality: '1080p',
+                    info: 'UAKino тестовий стрім'
+                }
+            ])
+        },
+        search: function(){},
+        add: function(){},
+        contextmenu: function(){}
+    }
+
+    if(window.Lampa) startPlugin()
+    else window.addEventListener('lampa', startPlugin)
 })();
