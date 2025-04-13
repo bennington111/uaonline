3b9287c2a427e31017ea2c8fb05324e49130cc04
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-13 23:49:11 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,38 +1,39 @@
 (function(){
-    function startPlugin(){
-        var manifest = {
-            type: 'video',
-            version: '1.0.0',
-            name: 'Онлайн UA Online',
-            description: 'Джерело з сайтів uakino та uaserials',
-            component: 'uaonline'
+    var manifest = {
+        type: 'video',
+        version: '1.0.0',
+        name: 'UA Online',
+        description: 'Тестовий плагін для Lampa',
+        component: 'uaonline'
+    };
+
+    function component(name){
+        if (name === 'uaonline') {
+            return {
+                item: function(object, resolve, reject) {
+                    resolve([{
+                        title: 'Тестовий потік',
+                        url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+                        quality: '1080p',
+                        info: 'Це тестовий стрім'
+                    }]);
+                },
+                search: function(){},
+                add: function(){},
+                contextmenu: function(){}
+            };
         }
+    }
 
+    if (window.Lampa && typeof Lampa.Plugin === 'function') {
         Lampa.Plugin(manifest, function(plugin){
-            plugin.component = function(name){
-                if(name === 'uaonline') return component
-            }
-
-            return plugin
-        })
+            plugin.component = component;
+        });
+    } else {
+        window.addEventListener('lampa', function(){
+            Lampa.Plugin(manifest, function(plugin){
+                plugin.component = component;
+            });
+        });
     }
-
-    var component = {
-        item: function(object, resolve, reject) {
-            resolve([
-                {
-                    title: 'UAkino (1080p)',
-                    url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-                    quality: '1080p',
-                    info: 'Тестовий потік UAKino'
-                }
-            ])
-        },
-        search: function(){},
-        add: function(){},
-        contextmenu: function(){}
-    }
-
-    if(window.Lampa) startPlugin()
-    else window.addEventListener('lampa', startPlugin)
 })();
