aff21fa6dd5cfb43ce43c472e6eec70c8b6113ab
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-14 00:10:48 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,39 +1,37 @@
-(function(){
-    var manifest = {
-        type: 'video',
-        version: '1.0.0',
-        name: 'UA Online',
-        description: 'Тестовий плагін для Lampa',
-        component: 'uaonline'
+(function () {
+    var uaonline = {
+        title: 'UAkino (1080p)',
+        url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+        quality: '1080p',
+        info: 'Тестовий потік UAKino'
     };
 
-    function component(name){
-        if (name === 'uaonline') {
-            return {
-                item: function(object, resolve, reject) {
-                    resolve([{
-                        title: 'Тестовий потік',
-                        url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-                        quality: '1080p',
-                        info: 'Це тестовий стрім'
-                    }]);
-                },
-                search: function(){},
-                add: function(){},
-                contextmenu: function(){}
+    function init() {
+        var manifest = {
+            type: 'video',
+            version: '1.0.0',
+            name: 'Онлайн UA Online',
+            description: 'Джерело з сайтів uakino та uaserials',
+            component: 'uaonline'
+        };
+
+        Lampa.Plugin(manifest, function (plugin) {
+            plugin.component = function (name) {
+                if (name === 'uaonline') return comp;
             };
-        }
+            return plugin;
+        });
     }
 
-    if (window.Lampa && typeof Lampa.Plugin === 'function') {
-        Lampa.Plugin(manifest, function(plugin){
-            plugin.component = component;
-        });
-    } else {
-        window.addEventListener('lampa', function(){
-            Lampa.Plugin(manifest, function(plugin){
-                plugin.component = component;
-            });
-        });
-    }
+    var comp = {
+        item: function (object, resolve, reject) {
+            resolve([uaonline]);
+        },
+        search: function () {},
+        add: function () {},
+        contextmenu: function () {}
+    };
+
+    if (window.Lampa) init();
+    else window.addEventListener('lampa', init);
 })();
