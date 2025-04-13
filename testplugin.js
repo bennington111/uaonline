2778ce99fe1ffb086b79301f7194a864bd437a2f
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-13 21:59:30 +0300

    Update testplugin.js

--- testplugin.js
+++ testplugin.js
@@ -1,35 +1,3 @@
 (function(){
-    var network = Lampa.Network;
-    var plugin = {
-        component: function(name){
-            if(name == 'uaonline') return component;
-        }
-    };
-
-    var component = {
-        item: function(object, resolve, reject){
-            resolve([
-                {
-                    title: 'UA Test (1080p)',
-                    url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-                    quality: '1080p',
-                    info: 'Тестовий стрім'
-                }
-            ]);
-        },
-        search: function(){},
-        add: function(){},
-        contextmenu: function(){}
-    };
-
-    Lampa.Plugin({
-        type: 'video',
-        name: 'Онлайн UA Online',
-        component: 'uaonline',
-        version: '1.0.0',
-        description: 'Тестовий UA Online плагін'
-    }, function(_) {
-        _.component = plugin.component;
-        return _;
-    });
+    alert('UAOnline плагін підключено!');
 })();
