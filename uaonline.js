a2c6e265065aba1e98a1b6cb3e4a4e9b49f7e95f
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-13 21:48:22 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,32 +1,38 @@
-(function () {
-    function startPlugin() {
+(function(){
+    function startPlugin(){
         var manifest = {
             type: 'video',
             version: '1.0.0',
             name: 'Онлайн UA Online',
-            description: 'UA джерело тест',
+            description: 'Тестовий клон online_mod для перевірки',
             component: 'uaonline'
-        };
+        }
 
-        Lampa.Platform.addSource(manifest, component);
+        Lampa.Plugin(manifest, function(plugin){
+            plugin.component = function(name){
+                if(name === 'uaonline') return component
+            }
+
+            return plugin
+        })
     }
 
     var component = {
-        item: function (object, resolve, reject) {
+        item: function(object, resolve, reject) {
             resolve([
                 {
-                    title: 'Тест UA 1080p',
+                    title: 'Тестовий потік (720p)',
                     url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-                    quality: '1080p',
-                    info: 'Демо джерело UA'
+                    quality: '720p',
+                    info: 'Тестове джерело'
                 }
-            ]);
+            ])
         },
-        search: function () {},
-        add: function () {},
-        contextmenu: function () {}
-    };
+        search: function(){},
+        add: function(){},
+        contextmenu: function(){}
+    }
 
-    if (window.Lampa) startPlugin();
-    else window.addEventListener('lampa', startPlugin);
+    if(window.Lampa) startPlugin()
+    else window.addEventListener('lampa', startPlugin)
 })();
