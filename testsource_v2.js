d798893357fa3a9359f9698967e2921a1ca1f2ce
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-14 09:24:47 +0300

    Update testsource_v2.js

--- testsource_v2.js
+++ testsource_v2.js
@@ -1,28 +1,38 @@
-// UAOnline testsource_v2.js
+console.log('[UAOnline]: testsource_v2.js завантажено');
 
 Lampa.Listener.follow('full', function (e) {
-  if (e.type === 'complite') {
-    var container = document.querySelector('.selectbox__content');
-    if (!container) {
-      console.log('UAOnline: контейнер не знайдено');
-      return;
+    console.log('[UAOnline: подія full ->]', e.type);
+
+    if (e.type === 'complite') {
+        waitForContainer();
     }
-
-    var button = document.createElement('div');
-    button.className = 'selectbox-item selectbox-item--icon selector';
-    button.innerHTML = `
-      <div class="selectbox-item__icon">🎬</div>
-      <div>
-        <div class="selectbox-item__title">UA Online</div>
-        <div class="selectbox-item__subtitle">testsource_v2</div>
-      </div>
-    `;
-
-    button.addEventListener('hover:enter', function () {
-      alert('UA Online кнопка натиснута!');
-    });
-
-    container.appendChild(button);
-    console.log('UAOnline: кнопка додана');
-  }
 });
+
+function waitForContainer(attempt = 0) {
+    const container = document.querySelector('.selectbox__content');
+
+    if (container) {
+        console.log('[UAOnline]: контейнер знайдено, додаю кнопку');
+
+        const button = document.createElement('div');
+        button.className = 'selectbox-item selectbox-item--icon selector';
+        button.innerHTML = `
+            <div class="selectbox-item__icon">🎬</div>
+            <div>
+                <div class="selectbox-item__title">UA Online</div>
+                <div class="selectbox-item__subtitle">testsource_v2</div>
+            </div>
+        `;
+
+        button.addEventListener('hover:enter', function () {
+            console.log('[UAOnline]: натиснуто кнопку UA Online');
+            Lampa.Noty.show('Натиснуто UA Online!');
+        });
+
+        container.appendChild(button);
+    } else if (attempt < 20) {
+        setTimeout(() => waitForContainer(attempt + 1), 200);
+    } else {
+        console.warn('[UAOnline]: контейнер кнопок не знайдено після очікування');
+    }
+}
