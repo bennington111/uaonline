fd0bf50787ff30da84fd96d93ce2d5628d66575e
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-15 21:47:25 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,58 +1,58 @@
 (function () {
-    if (!window.Lampa || !Lampa.Listener) return;
+    if (!window.Lampa || !Lampa.Listener || !Lampa.Activity) return;
 
-    console.log('[UAOnline] Плагін завантажено');
+    Lampa.Listener.follow('selectbox', function (e) {
+        // Спрацьовує тільки на відкриття меню "Джерело"
+        if (e.type === 'open' && e.data && e.data.title === 'Джерело') {
+            setTimeout(() => {
+                const body = document.querySelector('.selectbox__body .scroll__body');
+                if (!body || body.querySelector('[data-uaonline]')) return; // Уникаємо дубля
 
-    function observeSelectbox(callback) {
-        const observer = new MutationObserver(() => {
-            const el = document.querySelector('.selectbox');
-            if (el) {
-                observer.disconnect();
-                console.log('[UAOnline] Знайдено .selectbox');
-                callback(el);
-            }
-        });
+                const button = document.createElement('div');
+                button.className = 'selectbox-item selectbox-item--icon selector';
+                button.setAttribute('data-uaonline', 'true');
+                button.innerHTML = `
+                    <div class="selectbox-item__icon">
+                        <svg height="70" viewBox="0 0 80 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M71.2555 2.08955C74.6975 3.2397 77.4083 6.62804 78.3283 10.9306C80 18.7291 80 35 80 35C80 35 80 51.2709 78.3283 59.0694C77.4083 63.372 74.6975 66.7603 71.2555 67.9104C65.0167 70 40 70 40 70C40 70 14.9833 70 8.74453 67.9104C5.3025 66.7603 2.59172 63.372 1.67172 59.0694C0 51.2709 0 35 0 35C0 35 0 18.7291 1.67172 10.9306C2.59172 6.62804 5.3025 3.2395 8.74453 2.08955C14.9833 0 40 0 40 0C40 0 65.0167 0 71.2555 2.08955ZM55.5909 35.0004L29.9773 49.5714V20.4286L55.5909 35.0004Z" fill="currentColor"></path>
+                        </svg>
+                    </div>
+                    <div>
+                        <div class="selectbox-item__title">Онлайн UA Online</div>
+                        <div class="selectbox-item__subtitle">uaonline</div>
+                    </div>
+                `;
 
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
+                button.addEventListener('click', () => {
+                    Lampa.Activity.push({
+                        url: '',
+                        title: 'UA Online',
+                        component: 'uaonline',
+                        search: e.data.search,
+                        id: e.data.id,
+                        method: 'movie',
+                        card: e.data.card
+                    });
+                });
 
-        setTimeout(() => observer.disconnect(), 10000);
-    }
-
-    function addSourceButton(container) {
-        const btn = document.createElement('div');
-        btn.className = 'selectbox-item selectbox-item--icon selector';
-        btn.innerHTML = `
-            <div class="selectbox-item__icon">
-                <i class="fa fa-globe"></i>
-            </div>
-            <div class="selectbox-item__title">Онлайн UA Online</div>
-        `;
-
-        btn.addEventListener('click', () => {
-            console.log('[UAOnline] Клік по кнопці');
-            Lampa.Activity.push({
-                url: '',
-                title: 'UA Online',
-                component: 'online',
-                search: '',
-                search_one: '',
-                name: 'UA Online'
-            });
-        });
-
-        container.appendChild(btn);
-    }
-
-    Lampa.Listener.follow('activity', function (e) {
-        console.log('[UAOnline] Activity event:', e);
-
-        if (e.component === 'full' && e.type === 'start') {
-            console.log('[UAOnline] Повноекранна картка запущена');
-            observeSelectbox(addSourceButton);
+                body.appendChild(button);
+            }, 10); // Невелика затримка щоб DOM встиг зʼявитись
         }
     });
 
+    // Реєструємо пустий компонент uaonline, щоб Lampa не видала помилку
+    Lampa.Component.add('uaonline', {
+        create: function () {
+            this.render = function () {
+                let div = document.createElement('div');
+                div.innerHTML = '<div class="empty"><div class="empty__title">UA Online</div><div class="empty__text">Тут буде плеєр 😉</div></div>';
+                return div;
+            };
+        },
+        pause: function () {},
+        stop: function () {},
+        destroy: function () {}
+    });
+
+    console.log('UAOnline plugin loaded');
 })();
