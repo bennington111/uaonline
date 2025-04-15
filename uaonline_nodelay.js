ed813e9376edcf9eac0de7fee8bfeff9f28b9059
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-15 17:16:28 +0300

    Create uaonline_nodelay.js

---
+++ uaonline_nodelay.js
@@ -0,0 +1,27 @@
+(function () {
+    console.log('[UAOnline] Плагін завантажено');
+
+    Lampa.Module.add({
+        component: 'online',
+        name: 'Онлайн UA Online',
+        condition: () => true,
+        onSearch: function (query, callback) {
+            callback([]);
+        },
+        onCancel: function () {}
+    });
+
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type === 'complite') {
+            console.log('[UAOnline] Вставляємо кнопку');
+            e.object.appendSource({
+                title: 'UA Online',
+                url: '',
+                component: 'uaonline',
+                onClick: function () {
+                    console.log('[UAOnline] Натиснуто кнопку джерела');
+                }
+            });
+        }
+    });
+})();
