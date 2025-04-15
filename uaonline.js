ddc207678add86db71e45b72577cea60f3ed7231
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-15 21:13:03 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,34 +1,38 @@
 Lampa.Component.add('full', {
     onCreate: function (component) {
-        const container = component.render().find('.selectbox');
+        setTimeout(() => {
+            try {
+                const container = component.render().find('.selectbox');
 
-        if (!container.length) {
-            console.warn('[UAOnline] Не знайдено selectbox');
-            return;
-        }
+                if (!container || !container.length) {
+                    console.warn('[UAOnline] selectbox не знайдено');
+                    return;
+                }
 
-        const btn = $(`
-            <div class="selectbox-item selectbox-item--icon selector">
-                <div class="selectbox-item__icon">
-                    <i class="fa fa-globe"></i>
-                </div>
-                <div class="selectbox-item__title">Онлайн UA Online</div>
-            </div>
-        `);
+                const btn = $(`
+                    <div class="selectbox-item selectbox-item--icon selector">
+                        <div class="selectbox-item__icon"><i class="fa fa-globe"></i></div>
+                        <div class="selectbox-item__title">Онлайн UA Online</div>
+                    </div>
+                `);
 
-        btn.on('hover:enter', function () {
-            console.log('[UAOnline] Клік по кнопці');
-            Lampa.Activity.push({
-                url: '',
-                title: 'UA Online',
-                component: 'online',
-                search: '',
-                search_one: '',
-                name: 'UA Online'
-            });
-        });
+                btn.on('hover:enter', () => {
+                    console.log('[UAOnline] Клік на кнопку');
+                    Lampa.Activity.push({
+                        url: '',
+                        title: 'UA Online',
+                        component: 'online',
+                        search: '',
+                        search_one: '',
+                        name: 'UA Online'
+                    });
+                });
 
-        container.append(btn);
-        console.log('[UAOnline] Кнопка додана через Component.add');
+                container.append(btn);
+                console.log('[UAOnline] Кнопка успішно додана');
+            } catch (e) {
+                console.error('[UAOnline] Помилка при вставці кнопки:', e);
+            }
+        }, 1000); // даємо 1 секунду на рендер
     }
 });
