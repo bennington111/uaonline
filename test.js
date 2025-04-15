e4e3becec4dd7db7a7e0bc22a271337a29cd4bf3
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-15 16:07:02 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,34 +1,22 @@
 (function () {
-    function waitForModuleInit(callback) {
-        if (window.Lampa && Lampa.Module && typeof Lampa.Module.add === 'function') {
-            callback();
-        } else {
-            setTimeout(() => waitForModuleInit(callback), 500);
-        }
-    }
-
     function addSourceButton() {
+        // Ініціалізація online-модуля
         Lampa.Module.add({
             component: 'online',
             name: 'Онлайн UA Online',
             condition: () => true,
-            onItem: function (item, callback) {
-                // Просто повертаємо тестовий стрім
-                callback([{
-                    title: 'UA Stream',
-                    url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-                    timeline: '',
-                    quality: 'HD',
-                    subtitles: [],
-                    info: 'Тестовий UA Online'
-                }]);
-            },
             onSearch: function (query, callback) {
+                // Lampa потребує виклику callback хоча б раз
                 callback([]);
             },
-            onCancel: function () {}
+            onCancel: function () {},
+            onItem: function (element, data, callback) {
+                // Порожнє джерело — але це активує кнопку
+                callback([]);
+            }
         });
     }
 
-    waitForModuleInit(addSourceButton);
+    // Додаємо із затримкою, щоб всі залежності точно були ініціалізовані
+    setTimeout(addSourceButton, 2000);
 })();
