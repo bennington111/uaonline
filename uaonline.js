691320e7142331788f79af6ee9f0f2e329ae45e6
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-15 18:00:47 +0300

    Update uaonline.js

--- uaonline.js
+++ uaonline.js
@@ -1,41 +1,50 @@
-(function(){
-    function startPlugin(){
-        var manifest = {
-            type: 'video',
-            version: '1.0.0',
-            name: 'Онлайн UA Online',
-            description: 'Джерело з сайтів uakino та uaserials',
-            component: 'uaonline'
+(function () {
+    console.log('[UAOnline] Плагін завантажено');
+
+    function addButton(e) {
+        if (e.render.find('.uaonline--button').length) return;
+
+        const button = $('<div class="selectbox-item selectbox-item--icon selector uaonline--button">\
+            <div class="selectbox-item__icon"><svg width="16" height="16" viewBox="0 0 24 24"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h10v2H4z"/></svg></div>\
+            <div class="selectbox-item__title">Онлайн UA Online</div>\
+        </div>');
+
+        button.on('hover:enter', function () {
+            console.log('[UAOnline] Натиснуто кнопку UA Online');
+
+            Lampa.Activity.push({
+                url: '',
+                title: 'UA Online',
+                component: 'uaonline',
+                search: e.movie.title,
+                search_one: e.movie.title,
+                search_two: e.movie.original_title,
+                movie: e.movie,
+                page: 1
+            });
+        });
+
+        e.render.after(button);
+        console.log('[UAOnline] Кнопка додана');
+    }
+
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type === 'complite') {
+            addButton({
+                render: e.object.activity.render().find('.view--torrent'),
+                movie: e.data.movie
+            });
         }
+    });
 
-        Lampa.Plugin(manifest, function(plugin){
-            plugin.component = function(name){
-                if(name === 'uaonline') return component
-            }
-
-            return plugin
-        })
+    try {
+        if (Lampa.Activity.active().component === 'full') {
+            addButton({
+                render: Lampa.Activity.active().activity.render().find('.view--torrent'),
+                movie: Lampa.Activity.active().card
+            });
+        }
+    } catch (err) {
+        console.warn('[UAOnline] Помилка при додаванні кнопки:', err);
     }
-
-    var component = {
-        item: function(object, resolve, reject) {
-            resolve([
-                {
-                    title: 'UAkino (1080p)',
-                    url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-                    quality: '1080p',
-                    info: 'Тестовий потік UAKino'
-                }
-            ])
-        },
-        search: function(){},
-        add: function(){},
-        contextmenu: function(){},
-        id: 'uaonline',
-        type: 'video',
-        name: 'Онлайн UA Online'
-    }
-
-    if(window.Lampa) startPlugin()
-    else window.addEventListener('lampa', startPlugin)
 })();
