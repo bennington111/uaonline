0096807ff2548de7b1693ee2eb6592eb27139359
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-04-30 20:24:00 +0300

    Update uaflix_final_v3.js

--- uaflix_final_v3.js
+++ uaflix_final_v3.js
@@ -1,130 +1,111 @@
 // ==UserScript==
-// @name        Uaflix
-// @namespace   uaflix
-// @version     4.8
-// @description Плагін для перегляду фільмів з Uaflix
-// @author      YourName
-// @match       *://*/*
-// @grant       none
-// @icon        https://uafix.net/favicon.ico
+// @name Uaflix for Lampa
+// @namespace https://github.com/bennington111/
+// @version 4.2
+// @description Uaflix plugin for Lampa
+// @author Bennington
+// @match *://*/*
+// @icon https://uafix.net/favicon.ico
+// @grant none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    const CONFIG = {
-        name: 'Uaflix',
-        title: 'Uaflix',
-        version: '4.8',
-        url: 'https://uafix.net',
-        icon: 'https://uafix.net/favicon.ico',
-        buttonClass: 'view--uaflix_plugin'
-    };
-
-    // Надійне очікування елементів
-    function waitForElement(selector, timeout = 5000) {
-        return new Promise((resolve, reject) => {
-            const start = Date.now();
-            const check = () => {
-                const element = document.querySelector(selector);
-                if (element) {
-                    resolve(element);
-                } else if (Date.now() - start >= timeout) {
-                    reject(new Error(`Element ${selector} not found`));
-                } else {
-                    setTimeout(check, 100);
+lampa.plugin.add({
+    name: "uaflix",
+    init: function() {
+        this.addButton();
+    },
+    addButton: function() {
+        lampa.menu.add({
+            name: "Uaflix",
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M18 3H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm-1.5 13.5h-9v-9h9v9z"/></svg>',
+            color: "#ff5722",
+            action: () => {
+                this.loadUAFixContent();
+            }
+        });
+    },
+    loadUAFixContent: function() {
+        lampa.loader.show();
+        
+        let url = 'https://uafix.net/film/';
+        
+        lampa.request.get(url, (data) => {
+            if (!data) {
+                lampa.notice.show('Помилка завантаження Uaflix');
+                lampa.loader.hide();
+                return;
+            }
+            
+            const films = this.parseUAFix(data);
+            
+            if (films.length === 0) {
+                lampa.notice.show('Фільми не знайдені');
+                lampa.loader.hide();
+                return;
+            }
+            
+            this.showResults(films);
+            lampa.loader.hide();
+        });
+    },
+    parseUAFix: function(data) {
+        const films = [];
+        
+        const items = data.match(/<div class="video-item with-mask new-item">.*?<\/div><\/div>/gs);
+        
+        if (items) {
+            items.forEach(item => {
+                const titleMatch = item.match(/<div class="vi-title">(.*?)<\/div>/);
+                const title = titleMatch ? titleMatch[1].trim() : 'Невідома назва';
+                
+                const linkMatch = item.match(/<a class="vi-img img-resp-h" href="(.*?)"/);
+                const link = linkMatch ? 'https://uafix.net' + linkMatch[1] : '';
+                
+                const posterMatch = item.match(/<img src="(.*?)"/);
+                const poster = posterMatch ? 'https://uafix.net' + posterMatch[1] : '';
+                
+                if (title && link && poster) {
+                    films.push({
+                        title: title,
+                        link: link,
+                        poster: poster,
+                        year: this.extractYear(title),
+                        description: '',
+                        genre: ''
+                    });
                 }
-            };
-            check();
+            });
+        }
+        
+        return films;
+    },
+    extractYear: function(title) {
+        const yearMatch = title.match(/(\d{4})/);
+        return yearMatch ? yearMatch[0] : '';
+    },
+    showResults: function(films) {
+        const result = {
+            title: "Uaflix",
+            items: films.map(film => ({
+                title: film.title,
+                link: film.link,
+                poster: film.poster,
+                info: [
+                    film.year ? `Рік: ${film.year}` : '',
+                    film.genre ? `Жанр: ${film.genre}` : ''
+                ].filter(Boolean).join(' • '),
+                description: film.description || '',
+                action: {
+                    type: "open",
+                    value: film.link
+                }
+            }))
+        };
+        
+        lampa.pages.open('result', {
+            source: 'plugin',
+            title: 'Uaflix',
+            result: result
         });
     }
-
-    // Головний клас плагіна
-    class UaflixPlugin {
-        constructor() {
-            this.name = CONFIG.name;
-            this.type = 'online';
-            this.icon = CONFIG.icon;
-        }
-
-        exec(item, container) {
-            container.innerHTML = `
-                <div class="online-plugin__loading">
-                    <div class="online-plugin__loading-progress"></div>
-                    <div class="online-plugin__loading-text">Пошук на ${CONFIG.title}...</div>
-                </div>
-            `;
-
-            setTimeout(() => {
-                container.innerHTML = `
-                    <div class="online-plugin__empty">
-                        <div class="online-plugin__empty-icon">!</div>
-                        <div class="online-plugin__empty-title">Функціонал в розробці</div>
-                    </div>
-                `;
-            }, 1500);
-        }
-    }
-
-    // Ініціалізація плагіна
-    async function init() {
-        try {
-            // Чекаємо на завантаження Lampa
-            await new Promise(resolve => {
-                const checkLampa = () => {
-                    if (window.lampa && window.lampa.plugins) {
-                        resolve();
-                    } else {
-                        setTimeout(checkLampa, 100);
-                    }
-                };
-                checkLampa();
-            });
-
-            // Реєстрація плагіна
-            lampa.plugins.Uaflix = {
-                name: CONFIG.name,
-                component: UaflixComponent
-            };
-
-            // Чекаємо на контейнер кнопок
-            const container = await waitForElement('.full-start__buttons');
-            
-            // Додаємо кнопку
-            const button = `
-                <div class="full-start__button selector ${CONFIG.buttonClass}" data-subtitle="${CONFIG.title} ${CONFIG.version}">
-                    <svg width="24" height="24" viewBox="0 0 24 24">
-                        <path d="M3 3h18v2H3V3zm0 4h18v2H3V7zm0 4h12v2H3v-2zm0 4h12v2H3v-2zm0 4h12v2H3v-2z" fill="currentColor"/>
-                    </svg>
-                    <span>${CONFIG.title}</span>
-                </div>
-            `;
-            
-            container.insertAdjacentHTML('beforeend', button);
-            
-            // Обробник кліку
-            container.querySelector(`.${CONFIG.buttonClass}`).addEventListener('click', (e) => {
-                e.preventDefault();
-                const card = lampa.getCurrentCard();
-                if (card) {
-                    lampa.plugins.fullStartHide();
-                    lampa.plugins.exec(CONFIG.name, card);
-                }
-            });
-
-            console.log(`${CONFIG.title} v${CONFIG.version} successfully initialized`);
-        } catch (error) {
-            console.error('Initialization failed:', error);
-        }
-    }
-
-    // Запуск
-    if (document.readyState === 'complete') {
-        init();
-    } else {
-        window.addEventListener('load', init);
-    }
-
-    // Дублюючий компонент для сумісності
-    class UaflixComponent extends UaflixPlugin {}
-})();
+});
