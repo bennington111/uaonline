66960c5ae817f8871e460323faaa772b268993bc
Vitaly <36040586+bennington111@users.noreply.github.com>
2025-05-02 15:58:46 +0300

    Update uaflix_d.js

--- uaflix_d.js
+++ uaflix_d.js
@@ -1,7 +1,7 @@
 // ==UserScript==
  // @name        Uaflix
  // @namespace   uaflix
- // @version     1.3
+ // @version     1.4
  // @description Плагін для перегляду фільмів з Uaflix
  // @author      YourName
  // @match       *://*/*
@@ -12,45 +12,43 @@
 (function() {
     // Чекаємо готовності Lampa
     var interval = setInterval(function() {
-        if (typeof Lampa === 'undefined') return;
+        if (typeof lampa === 'undefined') return;
         clearInterval(interval);
 
-        // Реєстрація плагіна (як у uaflix_work.js)
-        Lampa.Plugin.add("uaflix", {
-            name: "UAFix",
-            version: "1.0",
-            icon: "https://uafix.net/favicon.ico"
-        });
+        // Реєстрація плагіна (як у online_mod.js)
+        lampa.plugins['uaflix_mod'] = {
+            name: 'UAFix',
+            version: '1.0',
+            icon: 'https://uafix.net/favicon.ico',
+            online: {
+                search: function(query) { return []; },
+                parse: function(url) { return []; }
+            }
+        };
 
-        // Точна копія функції додавання кнопки з uaflix_work.js
-        function addSourceButton() {
-            Lampa.Listener.follow('full', function(e) {
-                if (e.type === 'complite') {
-                    const button_html = `
-                    <div class="full-start__button selector view--uaflix" data-subtitle="UAFix 1.0">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 244 260" width="24" height="24" fill="currentColor">
-                            <path d="M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z
-                            M228.9,2l8,37.7l0,0L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z
-                            M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88L2,50.2L47.8,80L10,88z"/>
-                        </svg>
-                        <span>UAFix</span>
-                    </div>`;
+        // Додавання кнопки напряму (як у uaflix_work.js)
+        var checkInterval = setInterval(function() {
+            var container = document.querySelector('.full-start__buttons');
+            if (!container || container.querySelector('.view--uaflix')) return;
 
-                    const btn = $(button_html);
-                    btn.on('click', function() {
-                        if (Lampa.Storage.get('current_video')) {
-                            Lampa.Player.play({
-                                url: 'plugin://uaflix/parse?url=' + encodeURIComponent(Lampa.Storage.get('current_video').url),
-                                title: Lampa.Storage.get('current_video').title
-                            });
-                        }
-                    });
-
-                    $('.full-start__buttons').append(btn);
-                }
-            });
-        }
-
-        addSourceButton();
+            var button = document.createElement('div');
+            button.className = 'full-start__button selector selector--light view--uaflix';
+            button.innerHTML = `
+                <div class="selector__ico">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>
+                    </svg>
+                </div>
+                <div class="selector__name">UAFix</div>
+            `;
+            button.onclick = function(e) {
+                e.stopPropagation();
+                lampa.player.load({
+                    url: 'plugin://uaflix_mod/parse?url=' + encodeURIComponent(lampa.currentVideo.url),
+                    title: lampa.currentVideo.title
+                });
+            };
+            container.prepend(button);
+        }, 300);
     }, 100);
 })();
